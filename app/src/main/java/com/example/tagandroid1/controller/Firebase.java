3287caa3259b97c370e7a33b1ef55ce9729fc138
Guilherme Peixe <defftsu@gmail.com>
2019-11-21 18:17:06 -0300

    Authenticate using google sign-in

---
+++ app/src/main/java/com/example/tagandroid1/controller/Firebase.java
@@ -0,0 +1,4 @@
+package com.example.tagandroid1.controller;
+
+public class Firebase {
+}
--- app/src/main/java/com/example/tagandroid1/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/tagandroid1/ui/activity/LoginActivity.java
@@ -1,49 +1,125 @@
 package com.example.tagandroid1.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.tagandroid1.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     private FirebaseAnalytics mFirebaseAnalytics;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTitle("Sign in");
+        auth = FirebaseAuth.getInstance();
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        setLoginButtonConfig();
-    }
+        Button loginButton =  findViewById(R.id.activity_login_signin_button);
 
-
-    private void setLoginButtonConfig() {
-        Button loginButton = findViewById(R.id.activity_login_signin_button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isValidUser()){
-                    sendLoginEventToFirebase();
-                    openProductsActivity();
-                    Toast.makeText(LoginActivity.this, "Welcome back!", Toast.LENGTH_LONG).show();
-
-                }
-                else {
-                    Toast.makeText(LoginActivity.this, "User not valid!", Toast.LENGTH_LONG).show();
-                }
-
+                sendLoginEventToFirebase();
+                openProductsActivity();
+                finish();
+                Toast.makeText(getApplicationContext(), "User logged in successfully", Toast.LENGTH_LONG).show();
             }
         });
+
+        SignInButton loginGoogleButton = findViewById(R.id.activity_login_google_button);
+
+        loginGoogleButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 101);
+            }
+
+        });
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("SGF", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            sendLoginEventToFirebase();
+                            openProductsActivity();
+                            finish();
+                            Toast.makeText(getApplicationContext(), "User logged in successfully", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Could not log in user", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+
+
 
     private void openProductsActivity() {
         startActivity(new Intent(this, ProductsActivity.class));
@@ -70,12 +146,11 @@ public class LoginActivity extends AppCompatActivity {
         return false;
     }
 
-    public void sendLoginEventToFirebase(){
+    public void sendLoginEventToFirebase() {
             Bundle bundle = new Bundle();
             bundle.putString(FirebaseAnalytics.Param.METHOD, "Login");
             mFirebaseAnalytics.setUserId("u3sad341qfdasf");
             mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, bundle);
     }
-
 
 }
